f9f3639c8c3a44dae8c2985e6783610983adbf2d
Shloka-Patel <137433503+Shloka-Patel@users.noreply.github.com>
2024-08-01 00:37:18 +0530

    Add files via upload

---
+++ Shloka_B2_120_Exp5E_CDS.cpp
@@ -0,0 +1,22 @@
+// NAME - SHLOKA PATEL 
+// PRN - 23070123120 
+// EXPERIMENT - 5(E)    
+
+#include <iostream>
+using namespace std;
+int main()
+ {
+int day = 1;
+  switch (day) {
+    case 6:
+cout<< "Today is Saturday";
+      break;
+    case 7:
+cout<< "Today is Sunday";
+      break;
+    default:
+cout<< "Looking forward to the Weekend";
+  }
+  return 0;
+  }
+
